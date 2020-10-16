b986031f8735ad874b53ee3fcb043cb9df086f00
Everton <ton.oliveira@sempre.facig.edu.br>
2020-10-16 13:19:11 -0300

    Adicionado botão adicionar Orfanato

--- App.tsx
+++ App.tsx
@@ -1,22 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import MapView, { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
 
-import MapView from 'react-native-maps';
+import mapMarker from "./src/images/map-marker.png";
+
+import { useFonts } from "expo-font";
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from "@expo-google-fonts/nunito";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-  
-      <MapView 
-      style={styles.map}
-      initialRegion={{
-        latitude: -20.2580286,
-        longitude: -42.0295339,
-        latitudeDelta:0.008,
-        longitudeDelta:0.008,
-      }}
-       />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -20.2580286,
+          longitude: -42.0295339,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -20.2580286,
+            longitude: -42.0295339,
+          }}
+        >
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das Meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 resultados</Text>
+        <TouchableOpacity
+          style={styles.createOrphanageButton}
+          onPress={() => {}}
+        >
+          <Feather name="plus" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -24,11 +75,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
   },
 
-  map:{
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  }
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: "rgba(255, 255,255,0.8)",
+    borderRadius: 16,
+    justifyContent: "center",
+    fontFamily: "Nunito_700Bold",
+  },
+
+  calloutText: {
+    color: "#0086a5",
+    fontSize: 14,
+  },
+
+  footer: {
+    position: "absolute",
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    height: 46,
+    paddingLeft: 24,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+    elevation: 3,
+  },
+
+  footerText: {
+    color: "#8fa7b3",
+    fontFamily: "Nunito_700Bold",
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#15c3d6",
+    borderRadius: 20,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
